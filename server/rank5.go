756f33b338235845899e11ed8244f3f0c37a1088
dandyhuang <vivo@tx.com>
2022-07-29 18:22:44 +0800

    update

---
+++ server/rank5.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	pb "hello_grpc/api/protocol/mixer"
+	"log"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedRankServiceServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.RankRequest) (*pb.RankResponse, error) {
+	log.Printf("Received: %v", in)
+	return &pb.RankResponse{Version: in.Version}, nil
+}
+
+func main()  {
+	lis, err := net.Listen("tcp", "127.0.0.1:8090")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterRankServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
